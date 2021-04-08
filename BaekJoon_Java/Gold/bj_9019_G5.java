d56f4b2c46e235af1d049467b0dc74713645cb6a
HyeonJuSon <kkobukk95@gmail.com>
2021-04-08 13:50:12 +0900

    refactoring

--- BaekJoon_Java/Gold/bj_9019_G5.java
+++ BaekJoon_Java/Gold/bj_9019_G5.java
@@ -2,83 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class bj_9019_G5 {
-	static class Check{
-		public boolean isVisited;
-		public int parent;
-		public char register;
-		
-		public Check() {
-			isVisited = false;
-			parent = 0;
-			register=' ';
-		}
-		
+	static class Register{
+		public boolean isVisited = false;
+		public int parent = 0;
+		public char register =' ';
+		public Register() { }
 		public void set(boolean isVisited, int parent, char register) {
 			this.isVisited = isVisited;
 			this.parent = parent;
 			this.register = register;
 		}
 	}
-	static int A, B;
-	static final int max = 10000;
-	static Check[] checks;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		final int max = 10000;
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; ++tc) {
 			StringBuilder sb = new StringBuilder();
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
-			
-			checks = new Check[max];
-			for(int i=0;i<max;++i) checks[i] = new Check();
-
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			Register[] registers = new Register[max];
+			for(int i=0;i<max;++i) registers[i] = new Register();
 			Queue<Integer> q = new LinkedList<>();
 			q.add(A);
-			checks[A].isVisited = true;
-
+			registers[A].isVisited = true;
 			while (true) {
 				int current = q.poll();
-
-				int LtoR = current / 1000; // 맨 오른쪽으로 갈애
-				int RtoL = current % 10; // 맨 왼쪽으로 갈애
-
 				int D = (2 * current) % max;
 				int S = current == 0 ? max - 1 : current - 1;
-				int L = (current * 10 + LtoR) % max;
-				int R = 1000 * RtoL + current / 10;
+				int L = (current * 10 + (current / 1000)) % max;
+				int R = 1000 * (current % 10) + current / 10;
 				
-				if(!checks[D].isVisited) {
-					checks[D].set(true, current, 'D');
+				if(!registers[D].isVisited) {
+					registers[D].set(true, current, 'D');
 					q.add(D);
 				}
-				if(!checks[S].isVisited) {
-					checks[S].set(true, current, 'S');
+				if(!registers[S].isVisited) {
+					registers[S].set(true, current, 'S');
 					q.add(S);
 				}
-				if(!checks[L].isVisited) {
-					checks[L].set(true, current, 'L');
+				if(!registers[L].isVisited) {
+					registers[L].set(true, current, 'L');
 					q.add(L);
 				}
-				if(!checks[R].isVisited) {
-					checks[R].set(true, current, 'R');
+				if(!registers[R].isVisited) {
+					registers[R].set(true, current, 'R');
 					q.add(R);
 				}
 				// 찾았으면?
-				if(checks[B].isVisited) {
+				if(registers[B].isVisited) {
 					int tmp = B;
 					while(tmp != A) {
-						int pr = checks[tmp].parent;
-						sb.append(checks[tmp].register);
-						tmp = pr;
+						sb.append(registers[tmp].register);
+						tmp = registers[tmp].parent;
 					}
-					System.out.println(sb.reverse());
+					bw.write(sb.reverse().append('\n').toString());
 					break;
 				}
 			}
 		}
+		bw.close();
 	}
-
 }
