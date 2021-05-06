cb1d784f1a5b3bb7d131f175ba1c759a90605244
HyeonJuSon <kkobukk95@gmail.com>
2021-05-06 17:49:14 +0900

    2748,edit

--- BaekJoon_Java/Bronze/bj_2748_B1.java
+++ BaekJoon_Java/Bronze/bj_2748_B1.java
@@ -4,10 +4,10 @@ public class bj_2748_B1 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int[] fibo=new int[91];
+		long[] fibo=new long[91];
 		fibo[0] = 0;
 		fibo[1] = 1;
-		for(int i=2; i<=90; ++i) fibo[i] = fibo[i-1]+fibo[i-2];
+		for(int i=2; i<=N; ++i) fibo[i] = fibo[i-1]+fibo[i-2];
 		System.out.println(fibo[N]);
 	}
 }
