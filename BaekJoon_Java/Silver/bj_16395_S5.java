bb2c7514c3de8f6e2f6f217fb71df44a2ab74231
HyeonJuSon <kkobukk95@gmail.com>
2021-05-09 13:00:17 +0900

    bj 16395

--- BaekJoon_Java/Silver/bj_16395_S5.java
+++ BaekJoon_Java/Silver/bj_16395_S5.java
@@ -11,17 +11,12 @@ public class bj_16395_S5 {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		int[][] dp = new int[31][31];
-		dp[1][1] = 1;
-		dp[2][1] = dp[2][2] = 1;
-		int end = 3;
+		dp[1][1] = dp[2][1] = dp[2][2] = 1;
 		for (int i = 3; i <= N; ++i) {
-			for (int j = 1; j <= end; ++j) {
-				if (j == 1 || j == end)
-					dp[i][j] = 1;// 양끝
-				else
-					dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+			for (int j = 1; j <= i; ++j) {
+				if (j == 1 || j == i) dp[i][j] = 1;// 양끝
+				else dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
 			}
-			end++;
 		}
 		System.out.println(dp[N][K]);
 	}
