f4490eadd37b6fbaf41a0dd6841ff7c659c5884f
HyeonJuSon <kkobukk95@gmail.com>
2021-05-12 23:04:11 +0900

    bj 11718

---
+++ BaekJoon_Java/Bronze/bj_11718_B3.java
@@ -0,0 +1,13 @@
+package bronze;
+import java.util.*;
+public class bj_11718_B3 {
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+	}
+
+}
