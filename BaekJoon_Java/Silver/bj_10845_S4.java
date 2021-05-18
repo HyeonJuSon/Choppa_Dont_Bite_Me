a4b51461534db1b42ab22330ec12738dc2d20cd4
HyeonJuSon <kkobukk95@gmail.com>
2021-05-19 00:00:15 +0900

    bj 10845 ref

--- BaekJoon_Java/Silver/bj_10845_S4.java
+++ BaekJoon_Java/Silver/bj_10845_S4.java
@@ -1,57 +1,68 @@
 package silver;
+
 import java.io.*;
 import java.util.*;
+
 public class bj_10845_S4 {
-	static class Node{
+	static class Node {
 		int val;
 		Node next;
-		Node(int val){
+
+		Node(int val) {
 			this.val = val;
 		}
 	}
+
 	static Node head = null;
 	static int count = 0;
+
 	static void push(int val) {
 		Node newNode = new Node(val);
-		if(head==null) {
+		if (head == null) {
 			head = newNode;
-		}else {
+		} else {
 			Node tail = head;
-			while(tail.next!=null) tail= tail.next;
+			while (tail.next != null)
+				tail = tail.next;
 			tail.next = newNode;
 		}
 		++count;
 	}
+
 	static void pop() {
-		int val = head==null ?-1: head.val;
-		if(head.next!=null) {
-			head = head.next;
-		}else head = null;
-		--count;
+		int val = head == null ? -1 : head.val;
+		if (head != null) {
+			if (head.next != null) {
+				head = head.next;
+			} else
+				head = null;
+			--count;
+		}
 		System.out.println(val);
 	}
-	
+
 	static void isEmpty() {
-		System.out.println(count==0?1:0);
+		System.out.println(count == 0 ? 1 : 0);
 	}
-	
+
 	static void front() {
-		System.out.println(head==null?-1:head.val);
+		System.out.println(head == null ? -1 : head.val);
 	}
-	
+
 	static void back() {
 		Node tail = head;
-		while(tail.next!=null) tail = tail.next;
-		System.out.println(tail==null?-1:tail.val);
+		while (tail.next != null)
+			tail = tail.next;
+		System.out.println(tail == null ? -1 : tail.val);
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		for(int i=0;i<N;++i) {
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for (int i = 0; i < N; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			String order = st.nextToken();
-			switch(order) {
+			switch (order) {
 			case "push":
 				int val = Integer.parseInt(st.nextToken());
 				push(val);
