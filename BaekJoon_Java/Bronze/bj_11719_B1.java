8a88e37ba4a6fc30ce32880e599acdbe70bb4529
HyeonJuSon <kkobukk95@gmail.com>
2021-06-11 21:44:45 +0900

    bj 11719

---
+++ BaekJoon_Java/Bronze/bj_11719_B1.java
@@ -0,0 +1,11 @@
+package bronze;
+import java.util.*;
+public class bj_11719_B1 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()) System.out.println(sc.nextLine());
+		sc.close();
+	}
+
+}
