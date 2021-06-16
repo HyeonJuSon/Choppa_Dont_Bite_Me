26792512388643f7892a59a66e94d300beed9fa0
HyeonJuSon <kkobukk95@gmail.com>
2021-06-16 22:21:30 +0900

    refactoring 정규표현식

--- Programmers/prg_신규아이디추천_Lv1.java
+++ Programmers/prg_신규아이디추천_Lv1.java
@@ -1,16 +1,16 @@
 
 public class prg_신규아이디추천_Lv1 {
-	// Step1.
-	static String stepOne(String new_id) {
-		String lowerAlpha ="";//바꿀 문자를 저장할 변수
-		for(int i=0;i<new_id.length();++i) {
-			int tmp = (int)new_id.charAt(i);//문자열은 아스키코드로 저장되므로 int형으로변환
-			if(65<=tmp && tmp <=90) { // 글자가 대문자라면
-				lowerAlpha+=(char)(tmp+32);//소문자로 바꿔준다.
-			}else lowerAlpha+=(char)tmp;//그 외의 글자는 그냥 더해준다.
-		}
-		return lowerAlpha;
-	}
+	// Step1. -> 0616 주석처리
+//	static String stepOne(String new_id) {
+//		String lowerAlpha ="";//바꿀 문자를 저장할 변수
+//		for(int i=0;i<new_id.length();++i) {
+//			int tmp = (int)new_id.charAt(i);//문자열은 아스키코드로 저장되므로 int형으로변환
+//			if(65<=tmp && tmp <=90) { // 글자가 대문자라면
+//				lowerAlpha+=(char)(tmp+32);//소문자로 바꿔준다.
+//			}else lowerAlpha+=(char)tmp;//그 외의 글자는 그냥 더해준다.
+//		}
+//		return lowerAlpha;
+//	}
 	// Step2. 
 	static String stepTwo(String new_id) {
 		String delAlpha ="";
@@ -79,20 +79,29 @@ public class prg_신규아이디추천_Lv1 {
 	static String solution(String new_id) {
 		String answer="";
 //		1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
-		new_id = stepOne(new_id);
+		new_id = new_id.toLowerCase();// = stepOne(new_id);
 //		2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-		new_id = stepTwo(new_id);
+		new_id = new_id.replaceAll("[^a-z0-9-_.]","");//stepTwo(new_id);
 //		3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
-		new_id = StepThree(new_id);
+		new_id = new_id.replaceAll("\\.{2,}", ".");//StepThree(new_id);
 //		4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
-		new_id = StepFour(new_id);
+		new_id = new_id.replaceAll("^[.]|[.]$", "");//StepFour(new_id);
 //		5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-		new_id = StepFive(new_id);
+		new_id = new_id.length()==0?"a":new_id;
 //		6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
 //		     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-		new_id = StepFour(StepSix(new_id));
+		if(new_id.length()>=16) {
+			new_id = new_id.substring(0,15);
+			new_id = new_id.replaceAll("[.]$","");
+		}
+//		new_id = StepFour(StepSix(new_id));
 //		7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-		new_id = StepSeven(new_id);
+//		new_id = StepSeven(new_id);
+		if(new_id.length()<=2) {// 2글자 이하면
+			char last = new_id.charAt(new_id.length()-1);//마지막 문자를 
+			while(new_id.length()<3) // 3글자이상이 될 때까지 붙여주고
+				new_id+= last;
+		}
 		return answer = new_id;
 	}
 	
