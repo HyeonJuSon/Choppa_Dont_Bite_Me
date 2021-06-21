aac91b39bc16dbf0c7eefd798d461d048b92507d
HyeonJuSon <kkobukk95@gmail.com>
2021-06-21 22:36:18 +0900

    bj 10821

---
+++ BaekJoon_Java/Bronze/bj_10821_B2.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.io.*;
+import java.util.*;
+
+
+public class bj_10821_B2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(),",");
+		System.out.println(st.countTokens());
+	}
+
+}
