7de9fdf2b83c4ab41ef893cc5196532a3d1fb556
HyeonJuSon <kkobukk95@gmail.com>
2021-07-17 00:41:00 +0900

    bj 10757

---
+++ BaekJoon_Java/Bronze/bj_10757_B5.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class bj_10757_B5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = sc.nextBigInteger();
+		BigInteger B = sc.nextBigInteger();
+		System.out.println(A.add(B));
+	}
+
+}
