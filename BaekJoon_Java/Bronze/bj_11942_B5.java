3897fbf48b04694cdd7b2125d78ad33ea3f56eaf
HyeonJuSon <kkobukk95@gmail.com>
2021-07-21 22:41:10 +0900

    bj 1550 11942

---
+++ BaekJoon_Java/Bronze/bj_11942_B5.java
@@ -0,0 +1,9 @@
+package bronze;
+
+public class bj_11942_B5 {
+
+	public static void main(String[] args) {
+		System.out.println("고려대학교");
+	}
+
+}
---
+++ BaekJoon_Java/Bronze/bj_1550_B5.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_1550_B5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(Integer.parseInt(sc.next(),16));
+	}
+
+}
