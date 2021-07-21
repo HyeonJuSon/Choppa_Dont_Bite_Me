7f9cbc6504ae04572b00dbcb3ad6d1022bb56e2c
HyeonJuSon <kkobukk95@gmail.com>
2021-07-21 21:45:29 +0900

    bj 5338

---
+++ BaekJoon_Java/Bronze/bj_5338_G5.java
@@ -0,0 +1,13 @@
+package bronze;
+
+public class bj_5338_G5 {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
