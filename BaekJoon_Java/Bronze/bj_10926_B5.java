932964257dfc3fd04b7c1d9ef2b386f2314f0b31
HyeonJuSon <kkobukk95@gmail.com>
2021-07-22 22:32:06 +0900

    bj 10926

---
+++ BaekJoon_Java/Bronze/bj_10926_B5.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_10926_B5 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id+"??!");
+	}
+
+}
