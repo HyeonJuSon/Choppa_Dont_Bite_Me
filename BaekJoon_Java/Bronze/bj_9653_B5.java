bc98e064b5771d10e03c40ac2d1ffde960dc6e8a
HyeonJuSon <kkobukk95@gmail.com>
2021-07-22 22:05:13 +0900

    bj 9653

---
+++ BaekJoon_Java/Bronze/bj_9653_B5.java
@@ -0,0 +1,22 @@
+package bronze;
+
+public class bj_9653_B5 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+	    
+		sb.append("    8888888888  888    88888").append('\n')
+		  .append("   88     88   88 88   88  88").append('\n')
+		  .append("    8888  88  88   88  88888").append('\n')
+		  .append("       88 88 888888888 88   88").append('\n')
+		  .append("88888888  88 88     88 88    888888").append('\n')
+		  .append('\n')
+		  .append("88  88  88   888    88888    888888").append('\n')
+		  .append("88  88  88  88 88   88  88  88").append('\n')
+		  .append("88 8888 88 88   88  88888    8888").append('\n')
+		  .append(" 888  888 888888888 88  88      88").append('\n')
+		  .append("  88  88  88     88 88   88888888").append('\n');
+		System.out.println(sb.toString());
+	}
+
+}
