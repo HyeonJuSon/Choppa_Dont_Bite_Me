d7f89d72039cc55b182feb9fb4e1ddcc25bf5c8d
HyeonJuSon <kkobukk95@gmail.com>
2021-07-22 22:28:33 +0900

    bj 5339

---
+++ BaekJoon_Java/Bronze/bj_5339_B5.java
@@ -0,0 +1,20 @@
+package bronze;
+
+public class bj_5339_B5 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("     /~\\").append('\n')
+			.append("    ( oo|").append('\n')
+			.append("    _\\=/_").append('\n')
+			.append("   /  _  \\").append('\n')
+			.append("  //|/.\\|\\\\").append('\n')
+			.append(" ||  \\ /  ||").append('\n')
+			.append("============").append('\n')
+			.append("|          |").append('\n')
+			.append("|          |").append('\n')
+			.append("|          |").append('\n');
+		System.out.println(sb.toString());
+	}
+
+}
