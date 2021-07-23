d2382d52fa93d4a8399638d57d2ae8d6ea60268e
HyeonJuSon <kkobukk95@gmail.com>
2021-07-23 17:35:08 +0900

    bj 2420

---
+++ BaekJoon_Java/Bronze/bj_2420_B4.java
@@ -0,0 +1,14 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_2420_B4 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		long N =sc.nextLong();
+		long M =sc.nextLong();
+		System.out.println(Math.abs(N-M));
+	}
+
+}
