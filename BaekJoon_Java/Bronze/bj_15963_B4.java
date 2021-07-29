25ec5c81d9dee445dff0741f80abde8b0d1ff6a1
kkobu <kkobu@DESKTOP-IMIU0V3>
2021-07-30 01:05:19 +0900

    bj 15963

---
+++ BaekJoon_Java/Bronze/bj_15963_B4.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.*;
+
+public class bj_15963_B4 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc= new Scanner(System.in);
+		System.out.println(sc.nextLong() == sc.nextLong() ? 1:0);
+	}
+
+}
