0271080266307a7181dfe2d04cdd3fd88e2d658f
HyeonJu Son <kkobukk95@gmail.com>
2021-07-30 01:12:30 +0900

    bj 15963

--- BaekJoon_Java/Bronze/bj_15963_B4.java
+++ BaekJoon_Java/Bronze/bj_15963_B4.java
@@ -7,6 +7,7 @@ public class bj_15963_B4 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc= new Scanner(System.in);
 		System.out.println(sc.nextLong() == sc.nextLong() ? 1:0);
+		sc.close();
 	}
 
 }
