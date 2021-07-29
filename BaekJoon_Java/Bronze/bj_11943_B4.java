4017b6ee301efbaaa2b70f57b261434841a25748
kkobu <kkobu@DESKTOP-IMIU0V3>
2021-07-30 00:53:22 +0900

    bj 11943

---
+++ BaekJoon_Java/Bronze/bj_11943_B4.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.util.*;
+
+public class bj_11943_B4 {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc =new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		System.out.println(Math.min(A+D, B+C));
+	}
+
+}
