9b9cb3893ad2e7b68200501f49b234f1b36e91ab
kkobu <kkobukk95@gmail.com>
2021-08-11 22:58:02 +0900

    bj 3190 ing....

---
+++ BaekJoon_Java/Gold/bj_3190_G5.java
@@ -0,0 +1,23 @@
+package gold;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_3190_G5 {
+	
+	class Snake{
+		
+	}
+	
+	static int N, K, L, map[][];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		K = Integer.parseInt(br.readLine());
+		for(int i=0;i<K;++i) {
+			
+		}
+	}
+
+}
