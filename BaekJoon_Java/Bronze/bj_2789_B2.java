e34a2272a5571b4d4971b566f78a4a825bc1d01d
HyeonJu Son <kkobukk95@gmail.com>
2021-08-17 21:56:04 +0900

    bj 2789

---
+++ BaekJoon_Java/Bronze/bj_2789_B2.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_2789_B2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println(input.replaceAll("[CAMBRIDGE]",""));
+	}
+
+}
