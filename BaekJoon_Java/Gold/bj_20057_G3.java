ebf2018b900c142c8ad4a710ebb805821e40b74c
HyeonJu Son <kkobukk95@gmail.com>
2021-09-16 00:15:08 +0900

    bj 20057

--- BaekJoon_Java/Gold/bj_20057_G3.java
+++ BaekJoon_Java/Gold/bj_20057_G3.java
@@ -13,7 +13,7 @@ public class bj_20057_G3 {
 		}
 	}
 
-	static int N, map[][];
+	static int N, map[][], sum=0;
 	static Pos start;
 	static int[][] dir = { { 0, 1, 0, -1 }, { -1, 0, 1, 0 } }; // 좌 하 우 상 으로 움직임.
 
@@ -35,31 +35,39 @@ public class bj_20057_G3 {
 		start = new Pos(N / 2, N / 2);
 		int len = 1; // 1,1,2,2,3,3,...,N,N
 		int cnt = 0;
-		for (int d = 0; d < 4; ++d) {
-			for(int i=0;i<len;++i) {
-				int nx = start.x + dir[0][d];
-				int ny = start.y + dir[1][d];
-				System.out.println("현재 y의 위치");
-				if (isBoundary(nx, ny)) {
-					// 방향에따라 처리한다.
-					tornaido(d, nx, ny);
-					start.x = nx;
-					start.y= ny;
+		while(true) {
+			if(start.x==0 && start.y==0) break;
+			for (int d = 0; d < 4; ++d) {
+				for(int i=0;i<len;++i) {
+					if(start.x==0 && start.y==0) break;
+					int nx = start.x + dir[0][d];
+					int ny = start.y + dir[1][d];
+					System.out.println("현재 x의 위치 :"+start.x+", "+start.y);
+					System.out.println("현재 y의 위치 :"+nx+", "+ny);
+					if(nx==4&&ny==0) {
+						int a= 0;
+					}
+					if (isBoundary(nx, ny)) {
+						// 방향에따라 처리한다.
+						tornaido(d, nx, ny);
+						start.x = nx;
+						start.y= ny;
+					}
+				}
+				++cnt;
+				if(cnt==2) {
+					++len;
+					cnt=0;
 				}
 			}
-			++cnt;
-			if(cnt==2) {
-				++len;
-				cnt=0;
-			}
 		}
-		return 1;
+		return sum;
 	}
 	// 좌, 하(y*-1,x반대로), 우(x,y*-1), 상(y,x반대로)
 	static int[][] tor = { { 0, -1, 1, -2, -1, 1, 2, -1, 1 }, { -2, -1, -1, 0, 0, 0, 0, 1, 1 } }; 
 	
 	static double[] percent = { 0.05, 0.1, 0.1, 0.02, 0.07, 0.07, 0.02, 0.01, 0.01 };
-	static int[][] alphaDir = { { 0, 1, 0, -1 }, { -1, 0, 1, -1 } };
+	static int[][] alphaDir = { { 0, 1, 0, -1 }, { -1, 0, 1, 0 } };
 
 	static void tornaido(int d, int x, int y) {
 		int alpha = map[x][y];// a 값을 계산하기 위함
@@ -80,15 +88,28 @@ public class bj_20057_G3 {
 			}
 			int nx = x + torx;
 			int ny = y + tory;
-			int val = map[x][y] * (int) percent[t];
+			int val = (int)(map[x][y] * percent[t]);
+			System.out.print("비율 계산 - "+percent[t]+" ( "+nx+", "+ny+" ) = "+val);
 			alpha -= val;
 			if (isBoundary(nx, ny)) {
 				map[nx][ny] += val;
+			}else {
+				sum+=val;
+				System.out.print("=> 소실되었습니다 ");
 			}
+			System.out.println();
 		}
 		// y기준으로 알파값을 갱신해준다.
-		map[x + alphaDir[0][d]][y + alphaDir[1][d]] = alpha;
+		if(isBoundary(x+alphaDir[0][d],y+alphaDir[1][d]))
+			map[x + alphaDir[0][d]][y + alphaDir[1][d]] += alpha;
+		else {
+			System.out.println("알파값 소실되었습니다 : "+alpha);
+			sum+=alpha;
+		}
+		map[x][y]=0;//y위치의 모래는사라진다.
+		System.out.println("현재 소실된 모래 합:"+sum);
 		print();
+		System.out.println("====================");
 	}
 
 	static boolean isBoundary(int x, int y) {
