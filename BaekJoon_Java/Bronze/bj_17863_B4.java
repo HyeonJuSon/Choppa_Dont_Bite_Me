7b00dd9b622b8646093c26a753ac2f739ee33712
HyeonJu Son <kkobukk95@gmail.com>
2021-09-24 21:48:17 +0900

    bj 17863

---
+++ BaekJoon_Java/Bronze/bj_17863_B4.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_17863_B4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().substring(0,3).equals("555") ? "YES" : "NO");
+	}
+
+}
