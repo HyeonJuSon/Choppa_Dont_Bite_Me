55c362746e85747a3dd19875cd8fdd64b521c700
kkobu <kkobukk95@gmail.com>
2021-10-13 23:41:59 +0900

    [UPDATE] bj 21608

--- BaekJoon_Java/Silver/bj_21608_S1.java
+++ BaekJoon_Java/Silver/bj_21608_S1.java
@@ -9,47 +9,40 @@ public class bj_21608_S1 {
 		int sId;
 		int x, y; // 배치되었을 때의 위치
 		HashMap<Integer, Integer> likes = new HashMap<>();
-
 		public Student(int sId, int[] likes) {
 			this.sId = sId;
 			for (int i = 0; i < likes.length; ++i) {
 				this.likes.put(likes[i], 1);
 			}
-			this.x = this.y = -1;
+			this.x = this.y = -1; // 배치 되기 전이므로 -1로 둔다.
 		}
 	}
-
-	static int N, empty[][];
-	static ArrayList<Student> batchedStudent = new ArrayList<>();
-	static int[] score = { 0, 1, 10, 100, 1000 };
-
+	static int N, map[][];
+	static int[] score = { 0, 1, 10, 100, 1000 }; // 점수표
+	static int[][] dir = { { -1, 1, 0, 0 }, { 0, 0, -1, 1 } };
+	static ArrayList<Student> batchedStudent = new ArrayList<>(); // 배치된 학생들의 리스트
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		Queue<Student> students = new LinkedList<>();
 		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine());
+		Queue<Student> students = new LinkedList<>(); // 배치할 학생들
 		for (int i = 0; i < N * N; ++i) {
 			st = new StringTokenizer(br.readLine());
 			students.offer(new Student(stoi(st.nextToken()), new int[] { stoi(st.nextToken()), stoi(st.nextToken()),
 					stoi(st.nextToken()), stoi(st.nextToken()) }));
 		}
-		// 비어있는 곳을 체킹
-		empty = new int[N][N];
-		getEmptyBlock(empty);
-
-		// 1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸이 가장 많은 칸으로 정한다.
-		// 2. 1이 여러개인 경우 인접 칸에서 빈 칸이 가장 많은 칸을 선택한다.
-		// 3. 2가 여러개인 경우 행이 작은순, 열이 작은순으로 정한다.
+		// map을 인접 빈칸 갯수로 초기화한다.
+		map = new int[N][N];
+		initEmptyBlock(map);
+		// 학생들을 배치한다.
 		while (!students.isEmpty()) {
 			Student now = students.poll();
-			checkFavorite(now);
+			batchStudent(now);
 		}
-		// 4. 학생의 만족도 총 합을 출력한다.
+		// 학생의 만족도 총 합을 출력한다.
 		System.out.println(getScore());
 	}
-
-	static int[][] dir = { { -1, 1, 0, 0 }, { 0, 0, -1, 1 } };
-
+	
 	// 만족도의 총 합
 	static int getScore() {
 		int answer = 0;
@@ -58,30 +51,41 @@ public class bj_21608_S1 {
 			for (int d = 0; d < 4; ++d) {
 				int nx = s.x + dir[0][d];
 				int ny = s.y + dir[1][d];
-				if (isBoundary(nx, ny) && s.likes.containsKey(empty[nx][ny] * -1)) {
+				if(!isBoundary(nx,ny)) continue;
+				if (s.likes.containsKey(map[nx][ny] * -1)) {
 					++cnt;
 				}
 			}
-			answer += score[cnt];
+			answer += score[cnt]; // 점수를 합산한다.
 		}
 		return answer;
 	}
-
+	
+	// 주위 빈 칸의 개수를 반환
+	static int getEmptyCnt(int x, int y) {
+		int cnt = 0;
+		for(int d=0;d<4;++d) {
+			int nx = x+dir[0][d];
+			int ny = y+dir[1][d];
+			if(isBoundary(nx,ny) && map[nx][ny]>0) ++cnt;
+		}
+		return cnt;
+	}
+	
 	// 입력받은 배열에 주위 좋아하는 학생
-	static void checkFavorite(Student now) {
+	static void batchStudent(Student now) {
 		int max = -1, x = -1, y = -1;
 		if (batchedStudent.size() == 0) { // 배치된 학생이 없다면
 			// 빈칸이 가장 많은 곳에 넣는다.
 			for (int i = 0; i < N; ++i) {
 				for (int j = 0; j < N; ++j) {
-					if (max < empty[i][j]) {
-						max = empty[i][j];
+					if (max < map[i][j]) {
+						max = map[i][j];
 						x = i;
 						y = j;
 					}
 				}
 			}
-
 		} else { // 배치된 사람이 있다면
 			int[][] tmp = new int[N][N]; // 주위에 선호하는 사람의 수
 			for (Student s : batchedStudent) {
@@ -89,68 +93,59 @@ public class bj_21608_S1 {
 					for (int d = 0; d < 4; ++d) {
 						int nx = s.x + dir[0][d];
 						int ny = s.y + dir[1][d];
-						if (isBoundary(nx, ny) && empty[nx][ny] > 0) {
+						if (isBoundary(nx, ny) && map[nx][ny] > 0) {
 							tmp[nx][ny]++;
 						}
 					}
 				}
 			}
 			// 가장 큰 값 찾기
-			boolean isDupl = false;
-			Queue<int[]> big = new LinkedList<>();
+			boolean isDupl = false; // 여러개인 경우의 플래그
+			Queue<int[]> big = new LinkedList<>(); // 여러개일 때 비교할 대상을 담을 큐
 			for (int i = 0; i < N; ++i) {
 				for (int j = 0; j < N; ++j) {
-					if (tmp[i][j] >= 0 && empty[i][j]>0) {
+					if (map[i][j]>0) { // 이미 배치된 곳을 제외한 곳을 검사
 						if (max < tmp[i][j]) {
 							max = tmp[i][j];
 							x = i;
 							y = j;
 							isDupl=false;
-							big.clear();
+							big.clear(); // 초기화를 해주고 담아준다.
 							big.add(new int[] {i,j});
-						} else if (max == tmp[i][j]) {
+						} else if (max == tmp[i][j]) { // 같은 곳이 여러곳
 							big.add(new int[] {i,j});
-							isDupl = true; // 선호하는 학생 수가 같은 곳이 여러곳이면 더 검사할 필요 x
+							isDupl = true; // 플래그
 						}
 					}
 				}
 			}
 			if(isDupl) {
-				// 빈칸을 가장 많이 가진 것을 저장한다.
+				// 빈칸을 가장 많이 가진 것을 저장한다. 최상단 최좌측부터 담겨져있음. 2,3번 조건
 				max = -1;
 				while(!big.isEmpty()) {
 					int[] b = big.poll();
-					int bCnt = 0;
-					for(int d=0;d<4;++d) {
-						int nx = b[0]+dir[0][d];
-						int ny = b[1] +dir[1][d];
-						if(isBoundary(nx,ny) && empty[nx][ny]>0) ++bCnt;
-					}
-					if(max < bCnt) {
+					int bCnt = getEmptyCnt(b[0], b[1]);//주위 빈칸의 개수를 가져온다.
+					if(max < bCnt) { // 최초값만 저장하면되므로
 						max = bCnt;
 						x=b[0];
 						y=b[1];
 					}
 				}
-				
 			}
 		}
 		now.x = x;
 		now.y = y;
 		batchedStudent.add(now);
-		empty[x][y] = -1 * now.sId;
-		
+		map[x][y] = -1 * now.sId; // 음수로 학생의 번호를 저장해둔다.
 	}
 
 	// 입력받은 배열에 주위 빈칸의 개수를 반환
-	static void getEmptyBlock(int[][] map) {
+	static void initEmptyBlock(int[][] map) {
 		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < N; ++j) {
 				int cnt = 4;
-				if (i == 0 || i == N - 1)
-					--cnt;
-				if (j == 0 || j == N - 1)
-					--cnt;
+				if (i == 0 || i == N - 1) --cnt;
+				if (j == 0 || j == N - 1) --cnt;
 				map[i][j] = cnt;
 			}
 		}
