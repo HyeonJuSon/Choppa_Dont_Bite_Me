e1c8b9d7a905f35afb2159f377b293211f400fbf
HyeonJu Son <kkobukk95@gmail.com>
2021-10-23 16:30:10 +0900

    bj 1256

---
+++ BaekJoon_C++/bj_1256_G3.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int dp[101][101];
+string output;
+int check(int a, int z) {
+	if (a == 0 || z == 0) return 1;
+	if (dp[a][z] != 0) return dp[a][z];
+	return dp[a][z] = min(check(a - 1, z) + check(a, z - 1), 1000000001);
+}
+void find(int a, int z, int k) {
+	if (a == 0) {
+		for (int i = 0; i < z; ++i) output += 'z';
+		return;
+	}
+	if (z == 0) {
+		for (int i = 0; i < a; ++i) output += 'a';
+		return;
+	}
+	int val = check(a - 1, z);
+	if (val < k) {
+		output += 'z';
+		find(a, z - 1, k - val);
+	}
+	else {
+		output += 'a';
+		find(a - 1, z, k);
+	}
+}
+
+
+
+int main(void) {
+	int N, M,K;
+	cin >> N >> M >> K;
+	find(N, M, K);
+
+	if (check(N, M) < K) cout << -1 << '\n';
+	else cout << output << '\n';
+
+	return 0;
+}
