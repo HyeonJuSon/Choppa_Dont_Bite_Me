385f87d42a69f528f26bc14375c8afe55770e3fc
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2021-12-28 22:47:58 +0900

    [ALG] prg 짝수와 홀수

---
+++ Programmers/prg_짝수와홀수_LV1.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class prg_짝수와홀수_LV1 {
+	static public String solution(int num) {
+        return num%2==0 ? "Even":"Odd";
+    }
+	public static void main(String[] args) {
+		System.out.println(3);
+	}
+
+}
