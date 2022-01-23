effaedb2edc95d48bea83e1fb42a43a05f6f8337
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-01-23 20:02:37 +0900

    [ALG] bj 23288 ing

--- BaekJoon_Java/Gold/bj_23288_G3.java
+++ BaekJoon_Java/Gold/bj_23288_G3.java
@@ -8,6 +8,7 @@ public class bj_23288_G3 {
 	static int N, M, K, map[][], nowDirection;
 	static final int East = 0, West = 1, South = 2, North = 3;
 	static int[][] dice = { { 0, 2, 0 }, { 4, 1, 3 }, { 0, 5, 0 }, { 0, 6, 0 } };
+	static int maxCnt = 0, maxSum = 0;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,10 +24,13 @@ public class bj_23288_G3 {
 			}
 		}
 		nowDirection = East;
+		int answer = 0;
 		// K번 이동한다.
 		for (int k = 0; k < K; ++k) {
 			moveDice(0, 0); // 0,0에서 출발
+			answer = maxSum*dice[1][1];
 		}
+		System.out.println(answer);
 	}
 
 	// 주사위 이동 메소드
@@ -35,23 +39,30 @@ public class bj_23288_G3 {
 	static class Node {
 		int x, y, d;
 		int cnt;
+		int sum;
 
-		Node(int x, int y, int d, int cnt) {
+		Node(int x, int y, int d, int cnt, int sum) {
 			this.x = x;
 			this.y = y;
 			this.d = d;
 			this.cnt = cnt;
+			this.sum = sum;
 		}
 	}
 
 	static void moveDice(int x, int y) {
 
-		boolean[][] isVisited = new boolean[x][y];
+		boolean[][] isVisited = new boolean[N][M];
 		isVisited[x][y] = true;
 		Queue<Node> bfs = new LinkedList<>();
-		bfs.offer(new Node(x, y, nowDirection, 0));
+		bfs.offer(new Node(x, y, nowDirection, 0, 0));
+
 		while (!bfs.isEmpty()) {
 			Node current = bfs.poll();
+			if (maxCnt < current.cnt) {
+				maxCnt = current.cnt;
+				maxSum = Math.max(maxSum, current.sum);
+			}
 			int nx = x + dir[0][current.d];
 			int ny = y + dir[1][current.d];
 			if (!isBoundary(nx, ny)) {
@@ -65,18 +76,25 @@ public class bj_23288_G3 {
 				// 방문체크하고
 				isVisited[nx][ny] = true;
 				// bfsQ에 담는다
-				bfs.offer(new Node(nx, ny, changeDirection(current.d, map[nx][ny]), current.cnt + 1));
+				bfs.offer(new Node(nx, ny, changeDirection(current.d, map[nx][ny]), current.cnt + 1,
+						current.sum + map[nx][ny]));
 			}
 		}
 	}
+
 	static int changeDirection(int direction, int B) {
-		int A=dice[1][1];
-		if(A>B) {
-			
-		}else if(A<B) {
-			
+		int A = dice[1][1];
+		int newDirection = direction;
+		if (A > B) { // 시계방향90도
+			newDirection += 2;
+			if (direction == South || direction == North)
+				newDirection %= 4;
+		} else if (A < B) { // 반시계방향90도
+			newDirection -= 2;
+			if (direction == East || direction == West)
+				newDirection *= -1;
 		}
-		return direction;
+		return newDirection;
 	}
 
 	static boolean isBoundary(int x, int y) {
@@ -106,24 +124,27 @@ public class bj_23288_G3 {
 			break;
 		}
 	}
-	
+
 	static void rotateEast() {
 		int[][] tmp = new int[4][3];
 		tmp[0][1] = dice[0][1];
-		tmp[3][1] = dice[3][1];
-		
-		for(int j=1;j<3;++j) tmp[1][j-1]=dice[1][j];
-		tmp[1][2] = dice[3][1];
-		tmp[3][1] = dice[1][0];
+		tmp[2][1] = dice[2][1];
+		for (int j = 0; j < 2; ++j)
+			tmp[1][j + 1] = dice[1][j];
+		tmp[1][0] = dice[3][1];
+		tmp[3][1] = dice[1][2];
+		dice = tmp;
 	}
-	
+
 	static void rotateWest() {
 		int[][] tmp = new int[4][3];
 		tmp[0][1] = dice[0][1];
-		tmp[3][1] = dice[3][1];
-		for(int j=0;j<2;++j) tmp[1][j+1] = dice[1][j];
-		tmp[1][0] = dice[3][1];
-		tmp[3][1] = dice[1][2];
+		tmp[2][1] = dice[2][1];
+		for (int j = 1; j < 3; ++j)
+			tmp[1][j - 1] = dice[1][j];
+		tmp[1][2] = dice[3][1];
+		tmp[3][1] = dice[1][0];
+		dice = tmp;
 	}
 
 	static void rotateSouth() {
@@ -134,13 +155,13 @@ public class bj_23288_G3 {
 			tmp[i][2] = dice[i][2];
 		}
 		// 중앙은 한칸씩 위로간다.
-		for(int i=1;i<4;++i) {
+		for (int i = 1; i < 4; ++i) {
 			tmp[i][1] = dice[i][1];
 		}
 		tmp[3][1] = dice[0][1];
 		dice = tmp; // 갱신
 	}
-	
+
 	static void rotateNorth() {
 		int[][] tmp = new int[4][3];
 		// 전개도 왼, 우 는 똑같음
@@ -149,12 +170,11 @@ public class bj_23288_G3 {
 			tmp[i][2] = dice[i][2];
 		}
 		// 중앙은 한칸씩 아래로 간다
-		for(int i=0;i<3;++i) {
-			tmp[i+1][1] = dice[i][1];
+		for (int i = 0; i < 3; ++i) {
+			tmp[i + 1][1] = dice[i][1];
 		}
 		tmp[0][1] = dice[3][1];
 		dice = tmp;
 	}
 
-	
 }
