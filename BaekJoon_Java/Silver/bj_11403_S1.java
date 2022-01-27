17959be52eba3c8e2ecafdd14e5ef75570adebfc
SSAFY <ssafy_coach_78@ssafy.com>
2022-01-27 16:33:16 +0900

    [ALG] bj 11403

---
+++ BaekJoon_Java/Silver/bj_11403_S1.java
@@ -0,0 +1,38 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_11403_S1 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// step1. Input value
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; ++i) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < N; ++j) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		// step2. Check
+		for (int k = 0; k < N; ++k) {
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					if ((map[i][k] == 1 && map[k][j] == 1)) {
+						map[i][j] = 1;
+					}
+				}
+			}
+		}
+		// step3. Print
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
