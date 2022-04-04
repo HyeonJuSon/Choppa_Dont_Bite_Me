33e9b4bf89fb4d0b41bf9da283c88590e9683a49
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-04-04 19:21:52 +0900

    [ALG] bj 2738

---
+++ BaekJoon_Java/Bronze/bj_2738_B1.java
@@ -0,0 +1,33 @@
+package bronze;
+import java.io.*;
+import java.util.*;
+public class bj_2738_B1 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] A = new int[N][M];
+		int[][] B = new int[N][M];
+		for(int i=0;i<N;++i) {
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j=0;j<M;++j) {
+				A[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<N;++i) {
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j=0;j<M;++j) {
+				B[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0;i<N;++i) {
+			for(int j=0;j<M;++j) {
+				System.out.print((A[i][j] + B[i][j])+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
