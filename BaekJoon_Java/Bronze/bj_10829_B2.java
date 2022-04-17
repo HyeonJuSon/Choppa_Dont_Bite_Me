bd6b029e1eef1caa5878b468b020a847802035b4
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-04-17 22:29:41 +0900

    [ALG] bj 10829

---
+++ BaekJoon_Java/Bronze/bj_10829_B2.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class bj_10829_B2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Long N = sc.nextLong();
+		System.out.println(Long.toBinaryString(N));
+	}
+
+}
