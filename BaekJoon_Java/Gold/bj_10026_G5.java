b181ef62b63af9e28d2575274a380f1b02c73224
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-05-12 22:29:57 +0900

    [ALG] BJ 10026

--- BaekJoon_Java/Gold/bj_10026_G5.java
+++ BaekJoon_Java/Gold/bj_10026_G5.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class bj_10026_G5 {
 
+	static int[][] dir = { { -1, 1, 0, 0 }, { 0, 0, -1, 1 } };
 	static int[][] map;
 	static int N;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Int(br.readLine());
@@ -18,7 +18,6 @@ public class bj_10026_G5 {
 		}
 		System.out.println(process());
 	}
-
 	static String process() {
 		boolean[][] isVisited = new boolean[N][N];
 		int cnt = 0,rcnt =0;
@@ -43,9 +42,6 @@ public class bj_10026_G5 {
 		}
 		return cnt+" "+rcnt;
 	}
-
-	static int[][] dir = { { -1, 1, 0, 0 }, { 0, 0, -1, 1 } };
-
 	static void dfs(boolean rgb, int val,int i, int j, boolean[][] isVisited) {
 		for (int d = 0; d < 4; ++d) {
 			int ni = i + dir[0][d];
@@ -64,11 +60,9 @@ public class bj_10026_G5 {
 			}
 		}
 	}
-
 	static boolean isBoundary(int x, int y) {
 		return 0 <= x && x < N && 0 <= y && y < N;
 	}
-
 	static public int Int(String s) {
 		return Integer.parseInt(s);
 	}
