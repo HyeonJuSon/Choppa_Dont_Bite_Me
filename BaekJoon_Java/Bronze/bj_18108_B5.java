dc0f426351ad967a63db390240cb82951695d347
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-05-16 21:57:18 +0900

    [ALG] bj 18108

---
+++ BaekJoon_Java/Bronze/bj_18108_B5.java
@@ -0,0 +1,11 @@
+package bronze;
+import java.io.*;
+public class bj_18108_B5 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int from = Integer.parseInt(br.readLine());
+		System.out.println(from-543);
+	}
+
+}
