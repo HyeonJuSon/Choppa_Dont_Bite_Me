7aa9ea105b929d977fc26923ea9d3a60694555c9
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-05-18 14:13:49 +0900

    [ALG] bj 16638

--- BaekJoon_Java/Gold/BJ_16638_G1.java
+++ BaekJoon_Java/Gold/BJ_16638_G1.java
@@ -5,30 +5,26 @@ import java.util.*;
 
 public class BJ_16638_G1 {
 
-	static int N, number[];
-	static char oper[];
+	static int N;
 	static int max;
+	static String[] arr;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		max = Integer.MIN_VALUE; // 정답 값
-		number = new int[N]; // 숫자 담는 용
-		oper = new char[N]; // 연산자 담는 용
+		arr = new String[N];
 		String input = br.readLine();
 		// 01. 데이터 전처리
 		for (int i = 0; i < input.length(); ++i) {
-			char now = input.charAt(i);
-			if ('0' <= now && now <= '9')
-				number[i] = now - '0';
-			else
-				oper[i] = now;
+			arr[i] = input.charAt(i) + "";
 		}
 		// 02. 탐색
 		// 최대 뽑을 수 있는 괄호를 뽑아낸다. 연산자 수가 짝수면 N / 4 개 홀수면 N/4+1
-		int target = (N / 2) % 2 == 0 ? N / 4 : N / 4 + 1;
+		int operCnt = N / 2;
+		int target = operCnt % 2 == 0 ? operCnt / 2 : (operCnt / 2) + 1;
 		// R == 0개일 때 계산
-		calculate(new int[1]);
+		calculate(null);
 		// 그 외의 계산
 		for (int R = 1; R <= target; ++R) {
 			find(0, 1, R, new int[R]);
@@ -38,79 +34,68 @@ public class BJ_16638_G1 {
 	}
 
 	static void calculate(int[] select) {
-		// 연산자 방문 처리
-		boolean[] isVisited = new boolean[N];
-		int[] tmpNumber = new int[N];
+		// 임시배열에 복사
+		String[] nowArr = new String[N];
 		for (int i = 0; i < N; ++i)
-			tmpNumber[i] = number[i];
+			nowArr[i] = arr[i];
 		// 1. 괄호부터 계산한다.
-		for (int i = 0; i < select.length; ++i) {
-			if (select[i] != 0) {
-				int selectedIdx = select[i]; // 뽑은 인덱스를 추출
-				int front = selectedIdx - 1;
-				int back = selectedIdx + 1;
-				switch (oper[selectedIdx]) {
-				case '+':
-					tmpNumber[front] += tmpNumber[back];
-					break;
-				case '-':
-					tmpNumber[front] -= tmpNumber[back];
-					break;
-				case '*':
-					tmpNumber[front] *= tmpNumber[back];
-					break;
+		if (select != null) {
+			for (int i = 0; i < select.length; ++i) {
+				if (select[i] != 0) {
+					int selIdx = select[i]; // 뽑은 인덱스를 추출
+					int front = selIdx - 1;
+					int back = selIdx + 1;
+					int val = 0;
+					switch (nowArr[selIdx]) {
+					case "+":
+						val = Integer.parseInt(nowArr[front]) + Integer.parseInt(nowArr[back]);
+						break;
+					case "-":
+						val = Integer.parseInt(nowArr[front]) - Integer.parseInt(nowArr[back]);
+						break;
+					case "*":
+						val = Integer.parseInt(nowArr[front]) * Integer.parseInt(nowArr[back]);
+						break;
+					}
+					nowArr[selIdx] = Integer.toString(val);
+					nowArr[front] = nowArr[back] = null;
 				}
-				tmpNumber[back] = 0;
-				isVisited[selectedIdx] = true;
 			}
 		}
 		// 2. 곱하기를 계산한다.
 		for (int i = 1; i < N; i += 2) {
-			if (isVisited[i])
-				continue; // 이미 계산한 건 패스
-			if (oper[i] == '*') { // 곱하기일때만 계산
-				int leftIdx = i - 1, rightIdx = i + 1;
-				while (true) {
-					if(leftIdx - 2 < 0) break;
-					if(tmpNumber[leftIdx]==0) leftIdx-=2;
-					else break;
-				}
-				while (true) {
-					if(rightIdx + 2 >= N) break;
-					if(tmpNumber[rightIdx]==0) rightIdx+=2;
-					else break;
-				}
-				tmpNumber[leftIdx] *= tmpNumber[rightIdx];
-				tmpNumber[rightIdx] = 0;
-				isVisited[i] = true;
+			if (nowArr[i]!=null&&nowArr[i].equals("*")) { // 곱하기일때만 계산
+				int L = i - 1, R = i + 1;
+				while (L - 1 > 0 && nowArr[L] == null)
+					--L;
+				while (R + 1 < N && nowArr[R] == null)
+					++R;
+				nowArr[i] = Integer.toString(Integer.parseInt(nowArr[L]) * Integer.parseInt(nowArr[R]));
+				nowArr[L] = nowArr[R] = null;
 			}
 		}
 		// 3. 나머지를 계산한다.
 		for (int i = 1; i < N; i += 2) {
-			if(isVisited[i]) continue;
-			int leftIdx = i - 1, rightIdx = i + 1;
-			while (true) {
-				if(leftIdx - 2 < 0) break;
-				if(tmpNumber[leftIdx]==0) leftIdx-=2;
-				else break;
+			if (nowArr[i]==null) continue;
+			if (nowArr[i].equals("+") || nowArr[i].equals("-")) {
+				int L = i - 1, R = i + 1;
+				while (L - 1 > 0 && nowArr[L] == null)
+					--L;
+				while (R + 1 < N && nowArr[R] == null)
+					++R;
+				if(nowArr[i].equals("+")) {
+					nowArr[i] = Integer.toString(Integer.parseInt(nowArr[L]) + Integer.parseInt(nowArr[R]));
+				}else if(nowArr[i].equals("-")) {
+					nowArr[i] = Integer.toString(Integer.parseInt(nowArr[L]) - Integer.parseInt(nowArr[R]));
+				}
+				nowArr[L] = nowArr[R] = null;
 			}
-			while (true) {
-				if(rightIdx + 2 >= N) break;
-				if(tmpNumber[rightIdx]==0) rightIdx+=2;
-				else break;
-			}
-			if(oper[i]=='+') {
-				tmpNumber[leftIdx] += tmpNumber[rightIdx];
-			}else {
-				tmpNumber[leftIdx] -= tmpNumber[rightIdx];
-			}
-			tmpNumber[rightIdx] = 0;
-			isVisited[i] = true;
 		}
 		// 4. 총합을 계산한다.
 		int sum = 0;
-		for(int i=0;i<N;++i) {
-			sum += tmpNumber[i];
+		for (int i = 0; i < N; ++i) {
+			if (nowArr[i] != null)
+				sum += Integer.parseInt(nowArr[i]);
 		}
 		// 5. 갱신
 		max = Math.max(max, sum);
