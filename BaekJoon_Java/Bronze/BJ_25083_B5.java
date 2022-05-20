a5290ddb7055f7a617039f30ab39b612463660ae
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-05-20 21:53:29 +0900

    [ALG] bj 25083

---
+++ BaekJoon_Java/Bronze/BJ_25083_B5.java
@@ -0,0 +1,14 @@
+package bronze;
+
+public class BJ_25083_B5 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+
+}
