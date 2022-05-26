1793f9d329a0753acb80fbd3add6f6b7fe03b03b
DESKTOP-IMIU0V3\kkobu <kkobukk95@gmail.com>
2022-05-26 21:01:24 +0900

    [ALG] bj 5893

--- BaekJoon_Java/Bronze/bj_5893_B4.java
+++ BaekJoon_Java/Bronze/bj_5893_B4.java
@@ -1,15 +1,16 @@
-	package bronze;
-	
-	import java.util.Scanner;
-	
-	public class bj_5893_B4 {
-	
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			String bin = sc.next();
-			Long dec = Long.parseLong(bin, 2);
-			dec *= 17;
-			System.out.println(Long.toBinaryString(dec));
-		}
-	
+package bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class bj_5893_B4 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		BigInteger big = new BigInteger(input,2);
+		big = big.multiply(new BigInteger("17"));
+		System.out.println(big.toString(2));
 	}
+
+}
